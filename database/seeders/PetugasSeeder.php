b3a6f8ca232f35c963d04c727b8327f9395c5827
Vinluvy <123059312+VinLvy@users.noreply.github.com>
2025-01-24 09:57:52 +0700

    Seeder

---
+++ database/seeders/PetugasSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class PetugasSeeder extends Seeder
+{
+    public function run()
+    {
+        DB::table('petugas')->insert([
+            [
+                'nama_petugas' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('admin123'),
+                'role' => 'administrator',
+            ],
+            [
+                'nama_petugas' => 'Petugas',
+                'email' => 'petugas@gmail.com',
+                'password' => Hash::make('petugas123'),
+                'role' => 'petugas',
+            ],
+            [
+                'nama_petugas' => 'Kasir',
+                'email' => 'kasir@gmail.com',
+                'password' => Hash::make('kasir123'),
+                'role' => 'kasir',
+            ],
+        ]);
+    }
+}
