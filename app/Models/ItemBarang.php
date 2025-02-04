2777548a5f1b001ca1226da1e545273be7191d8b
Vinluvy <123059312+VinLvy@users.noreply.github.com>
2025-02-04 12:41:15 +0700

    Models 1.2

--- app/Models/ItemBarang.php
+++ app/Models/ItemBarang.php
@@ -24,6 +24,22 @@ class ItemBarang extends Model
         'kategori_id',
     ];
 
+    protected static function boot()
+    {
+        parent::boot();
+
+        // Event creating akan dipanggil sebelum data disimpan ke database
+        static::creating(function ($item) {
+            // Ambil kode barang terakhir
+            $lastItem = self::orderBy('id', 'desc')->first();
+            $lastCode = $lastItem ? $lastItem->kode_barang : 'BRG000';
+
+            // Generate kode barang baru
+            $number = (int) substr($lastCode, 3) + 1; // Ambil angka dari kode terakhir dan tambahkan 1
+            $item->kode_barang = 'BRG' . str_pad($number, 3, '0', STR_PAD_LEFT); // Format kode barang
+        });
+    }
+
     public function kategori()
     {
         return $this->belongsTo(KategoriBarang::class, 'kategori_id');
